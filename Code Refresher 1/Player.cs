77ab4a62365278814a220a649ba62d5e72696743
NotNewAnymore <Henryfleischer01@gmail.com>
2023-09-29 18:51:34 -0700

    Add project files.

---
+++ Code Refresher 1/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Refresher_1
+{
+	internal class Player
+	{
+		private string name;
+		private int number;
+		public string Name
+		{
+			get { return name; }
+		}
+		public Player(string name, int number)
+		{
+			this.name = name;
+			this.number = number;
+		}
+		public override string ToString()
+		{
+			return $"Name: {name}| ID: {number}";
+		}
+	}
+}
---
+++ Code Refresher 1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Code_Refresher_1
+{
+	internal class Program
+	{
+		public static List<Player> playerslist = new List<Player> { new Player("James", 0), new Player("Jackson", 1), new Player("Jill", 2), new Player("Johnathan", 3), new Player("Jasmine", 4) };
+		static void Main(string[] args)
+		{
+			//Automatically add a player to the list
+			playerslist.Add(new Player("Terry", 5));
+			//Print odd numbers in list
+			for (int i = 0; i < playerslist.Count; i++)
+			{
+				if (i % 2 == 1)
+				{
+					Console.WriteLine(playerslist.ElementAt(i));
+				}
+			}
+			Console.WriteLine("Enter a letter to search for");
+			//Get player input
+			string input = Console.ReadLine();
+			//Print names that start with player input
+			foreach (Player player in playerslist)
+			{
+				if (player.Name[0] == input[0])
+				{
+					Console.WriteLine(player);
+				}
+			}
+			//Add player to the list
+			Console.WriteLine("Enter a name");
+			string name = Console.ReadLine();
+			Console.WriteLine("Enter a number");
+			
+			int ID = Convert.ToInt32(Console.ReadLine());
+			Player newplayer = new Player(name,ID);
+			playerslist.Add(newplayer);
+
+			foreach (Player player in playerslist)
+			{
+				Console.WriteLine(player);
+			}
+
+		}
+	}
+}
